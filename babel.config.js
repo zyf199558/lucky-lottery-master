40e84a1cc14846ce1102a88c7f25f0b07f94f6fb
root <root@app-server>
2024-11-24 00:49:18 +0800

    first commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env']
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+ "@babel/plugin-transform-runtime"
+
+  ]
+};
